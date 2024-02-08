136747fd10d8103496c2eff286e0891f67f32a12
daytona65 <niclim65@gmail.com>
2024-02-08 22:42:22 +0800

    Update Login UI

--- client/Login.jsx
+++ client/Login.jsx
@@ -1,35 +1,54 @@
 import { useState } from 'react';
-import { ImageBackground, KeyboardAvoidingView, StyleSheet, Text, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { Image, KeyboardAvoidingView, StyleSheet, Text, TouchableWithoutFeedback, Keyboard, View } from 'react-native';
 import { Button, TextInput } from 'react-native-paper';
 import { COLORS } from './constants/theme';
+import Background from './assets/drawkit-transport-scene-5.png';
 
 export default function Login({navigation}) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   return (
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()} style={{backgroundColor: 'red'}}>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <KeyboardAvoidingView behavior="padding" style={styles.container}>
-        <ImageBackground>
+      <Image source={{uri: 'https://picsum.photos/200/300'}} resizeMode='repeat'/>
           <Text style={styles.title}>Welcome, Groundhog!</Text>
           <TextInput 
-          style={styles.input} 
-          label="Username" 
-          value={username} 
-          onChangeText={text => setUsername(text)} 
+            style={styles.input} 
+            label="Username" 
+            value={username} 
+            onChangeText={text => setUsername(text)} 
           />
 
           <TextInput 
-          style={styles.input} 
-          label="Password" 
-          value={password} 
-          onChangeText={text => setPassword(text)}
-          secureTextEntry={true} 
+            style={styles.input} 
+            label="Password" 
+            value={password} 
+            onChangeText={text => setPassword(text)}
+            secureTextEntry={true} 
           />
-          <Button style={styles.button} name="Login" onPress={()=> navigation.navigate('Tabs')}>
-            Login
-          </Button>
-        </ImageBackground>
+          <Text style={{color: 'blue', textDecorationLine: 'underline'}}
+            onPress={()=> navigation.navigate('Tabs')}>
+              Forgot Password?
+            </Text>
+          <View style={styles.buttonContainer}>
+            <Button 
+              style={styles.button} 
+              name="Login" 
+              textColor='white'
+              onPress={()=> navigation.navigate('Tabs')}
+            >
+              Login
+            </Button>
+            <Button 
+              style={styles.button} 
+              name='Sign up'
+              textColor='white'
+              onPress={()=> navigation.navigate('Tabs')}
+            >
+              Sign Up
+            </Button>
+          </View>
       </KeyboardAvoidingView>
     </TouchableWithoutFeedback>
 
@@ -45,6 +64,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginBottom: 50
   },
   separator: {
     marginVertical: 30,
@@ -55,8 +75,15 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     width: 300
   },
-
+  buttonContainer: {
+    flexDirection: 'row',
+    alignContent: 'center',
+    marginTop: 50,
+    justifyContent: 'space-around',
+  },
   button: {
     backgroundColor: COLORS.primary,
+    margin: 10,
+    width: 100,
   }
 });
